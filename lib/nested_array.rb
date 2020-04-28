84acd69e050aabf0ae6241045109348a06854b44
Akiko Green <akikogreen08@gmail.com>
2020-04-28 19:31:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,7 +18,8 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   
-  STORa
+  STORAGE = [
+    ]
   
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
